52e8e0618b3f0a2881b616293ddfad79e58d1f12
Randy Kim <rhkim1292@gmail.com>
2022-06-27 18:56:05 -0700

    Fix the grid so the tiles align properly
    The grid wasn't displaying 16 tile rows by 16 tile columns before so
    the javascript file and the css file had to be adjusted to make the
    necessary calculations in order for the tiles to fit in the browser's
    viewport properly.

--- script.js
+++ script.js
@@ -5,15 +5,18 @@ createGrid(gridSize, gridContainer);
 function createGrid(gridSize, gridContainer) {
     let gridArea = gridSize * gridSize;
     let tile;
+    let containerWidth = gridContainer.clientWidth - 15;
+    let widthOfTile = containerWidth / gridSize;
+    let tileToContainerWidthRatio = (widthOfTile / containerWidth) * 100;
 
     for (let i = 0; i < gridArea; i++) {
         tile = Object.assign(document.createElement("div"), {
             className: `tile-${i + 1}`,
-            textContent: `tile ${i + 1}`,
         });
         tile.style.backgroundColor = "white";
-        tile.style.borderStyle = "solid";
+        tile.style.borderStyle = "none";
         tile.style.borderColor = "red";
+        tile.style.flex = `1 1 ${tileToContainerWidthRatio}%`;
         gridContainer.appendChild(tile);
     }
 }
