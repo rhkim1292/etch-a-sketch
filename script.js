f5a075ab0acfefdd5aec647019f15f07f29369a3
Randy Kim <rhkim1292@gmail.com>
2022-06-27 23:12:02 -0700

    Implement change resolution button functionality
    The change resolution button now displays a prompt when clicked.
    The prompt asks the user to input a custom resolution for the grid.
    This will allow the user to play with a grid of variable resolutions.

--- script.js
+++ script.js
@@ -1,5 +1,23 @@
-let gridSize = 16;
+const DEFAULT_GRID_SIZE = 16
+let gridSize = DEFAULT_GRID_SIZE;
+
 const gridContainer = document.querySelector(".grid-container");
+const resolutionBtn = document.getElementById("resolutionBtn");
+
+resolutionBtn.addEventListener("click", (e) => {
+    gridSize = prompt("Choose a grid size from 1 to 100");
+    gridSize = parseInt(gridSize);
+    console.log(gridSize);
+    if (!gridSize || gridSize > 100 || gridSize < 1) {
+        alert("Are you kidding?! That's not a valid input!");
+        gridSize = DEFAULT_GRID_SIZE;
+        return;
+    }
+
+    gridContainer.replaceChildren(); // Removes all children of grid container
+    createGrid(gridSize, gridContainer);
+});
+
 createGrid(gridSize, gridContainer);
 
 function createGrid(gridSize, gridContainer) {
